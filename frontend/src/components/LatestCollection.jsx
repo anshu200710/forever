4a2c63dc7780490d586022fe737c33ae72a193e6
anshu <anshu200710>
2025-04-14 19:15:52 +0530

    product show on top

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
 import ProductItem from './ProductItem';
 
@@ -9,16 +9,15 @@ const LatestCollection = () => {
 
     useEffect(() => {
         if (products && products.length > 0) {
-          const reversedProducts = [...products].reverse();
-          setLatestProducts(reversedProducts.slice(0, 10));
+            // 1. Create a copy and then reverse it
+            const reversedProducts = [...products].reverse();
+            // 2. Slice the first 10 items
+            setLatestProducts(reversedProducts.slice(0, 10));
         } else {
-          setLatestProducts([]); 
+            setLatestProducts([]);
         }
-      }, [products]);
+    }, [products]);
 
-
-
-    
     return (
         <div className='my-10'>
             <div className='text-center py-8 text-3xl'>
@@ -32,14 +31,12 @@ const LatestCollection = () => {
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
                 {
                     latestProducts.map((item, index) => (
-                       // Add parentheses for implicit return or use {return ...;}
-                      <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}  />
+                        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
                     ))
                 }
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default LatestCollection
+export default LatestCollection;
